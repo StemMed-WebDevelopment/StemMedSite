7460d923b71b4297791f8cd2acb16423c329de6e
StemMed-WebDevelopment <stemmedproject@gmail.com>
2025-07-20 16:30:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const blocks = document.querySelectorAll('.content-block');
+
+function revealOnScroll() {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  blocks.forEach(block => {
+    const blockTop = block.getBoundingClientRect().top;
+    if(blockTop < triggerBottom) {
+      block.classList.add('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
